f32df116c843dbaa80c7dc17f4e5c2852b015a7b
Peter Schmidt <schmidt.multimedia@gmail.com>
2026-01-12 14:25:21 +0000

    04 / Codechallenge I - Lösungsbeispiel

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -11,3 +11,49 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 
 const prompt = require('prompt-sync')({sigint: true});
 
+/*********** Variante 1  ****************/
+// Vorteil: gute Lesbarkeit | aber viele Variablen
+
+// let ziffer1, ziffer2, zahl1, zahl2, summe;
+
+// ziffer1 = prompt("Bitte Zahl 1 eingeben: ");
+// ziffer2 = prompt("Bitte Zahl 2 eingeben: ");
+
+// console.log("Datentyp: " + typeof ziffer1);
+// console.log("Datentyp: " + typeof ziffer2);
+
+// // Typkonvertierung 1: ziffer --> zahl (mit Nachkomma)
+// zahl1 = parseFloat(ziffer1);
+// zahl2 = parseFloat(ziffer2);
+
+// console.log("Datentyp: " + typeof zahl1);
+// console.log("Datentyp: " + typeof zahl2);
+
+// summe = zahl1 + zahl2;
+
+// console.log("Die Summe der Zahlen ist: " + summe);
+
+/*********** Variante 2  ****************/
+
+// Kompaktere Lösung
+
+// let zahl1, zahl2, summe;
+
+// zahl1 = parseInt(prompt("Zahl 1: "));
+// zahl2 = parseInt(prompt("Zahl 2: "));
+
+// summe = zahl1 + zahl2;
+
+// console.log("Die Summe der Zahlen ist: " + summe);
+
+/*********** Variante 3  ****************/
+// Kompakt, aber schlechter lesbar, unflexibel
+
+// console.log
+// (
+//     "Die Summe ist: " + 
+//    (
+//     parseFloat(prompt("Bitte Zahl 1 eingeben: ")) +
+//     parseFloat(prompt("Bitte Zahl 2 eingeben: "))
+//    ) 
+// );
