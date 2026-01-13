f0b2e078c0861dfb40346b023592ddfefcec3b7d
Peter Schmidt <schmidt.multimedia@gmail.com>
2026-01-13 09:49:55 +0000

    05 / Iterationen I - III

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,32 @@
+
+/* Berechnung Alter */
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 1980;
+const birthYearMark = 1990;
+
+// Berechnung Alter - Iteration I
+// let year = 2026;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+
+// Berechnung Alter - Iteration II
+// let date = new Date();
+// console.log("Datum: " + date);
+// let year = date.getFullYear();
+// console.log("Jahr: " + year);
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter - Iteration III
+let year = new Date().getFullYear();
+console.log("Jahr: " + year);
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
