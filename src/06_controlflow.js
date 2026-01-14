6be85e2ed82bb977de1a450614c75300daebc324
Peter Schmidt <schmidt.multimedia@gmail.com>
2026-01-14 10:25:53 +0000

    06 / Fallunterscheidung / SWITCH|CASE 2

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -68,36 +68,53 @@ console.log("------------------");
 
 /****** Fallunterscheidung / SWITCH|CASE 1 ******/
 
-const firstName = "Jane";
-let job;
+// const firstName = "Jane";
+// let job;
 
-job = "driver";      // .. fährt TAXI! / UBER
-job = "diver";       // .. taucht im Rhein!
-job = "artist";      // .. malt ein Bild!
-job = "pilot";       // .. macht etwas anderes! --> default
-job = "teacher";     // .. unterrichtet!
-job = "instructor";  // .. unterrichtet!
+// job = "driver";      // .. fährt TAXI! / UBER
+// job = "diver";       // .. taucht im Rhein!
+// job = "artist";      // .. malt ein Bild!
+// job = "pilot";       // .. macht etwas anderes! --> default
+// job = "teacher";     // .. unterrichtet!
+// job = "instructor";  // .. unterrichtet!
 
-switch (job) 
-{
-    case "driver":
-        console.log(firstName + " fährt TAXI!");
+// switch (job) 
+// {
+//     case "driver":
+//         console.log(firstName + " fährt TAXI!");
+//         break;
+
+//     case "diver":
+//         console.log(firstName + " taucht im Rhein!");
+//         break;
+
+//     case "artist":
+//         console.log(firstName + " malt ein Bild!");
+//         break;
+
+//     case "teacher":
+//     case "instructor":
+//         console.log(firstName + " unterrichtet!");
+//         break;
+
+//     default: // WICHTIG !!!!
+//         console.log(firstName + " macht etwas anderes!");
+//         break;
+// }
+
+/******** Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const value = 10;
+
+switch (cond) {
+    case (value == 1):
+        console.log("case 1.");
         break;
-
-    case "diver":
-        console.log(firstName + " taucht im Rhein!");
+    case (value == 2):
+        console.log("case 2.");
         break;
-
-    case "artist":
-        console.log(firstName + " malt ein Bild!");
-        break;
-
-    case "teacher":
-    case "instructor":
-        console.log(firstName + " unterrichtet!");
-        break;
-
-    default: // WICHTIG !!!!
-        console.log(firstName + " macht etwas anderes!");
+    default:
+        console.log("Value hat einen anderen Wert.");
         break;
 }
