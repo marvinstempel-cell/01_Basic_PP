237cef2514c41bf7630e6b342da6554bf74d0bf4
Peter Schmidt <schmidt.multimedia@gmail.com>
2026-01-16 11:58:31 +0000

    09 / Codechallenge III /Aufgabe

---
+++ src/09_codechallenge_III.js
@@ -0,0 +1,15 @@
+
+/***** Codechallenge III *****/
+
+// ControlFlow / Funktionen
+
+// Erstellen Sie ein JS-Programm, dass einen Bestellvorgane bei MD
+// simuliert. Orientieren Sie sich dabei am Struktogramm in der Datei
+// bestellung.json  - Datei bitte in den online-Editor STRUCTOG laden
+// https://ddi.education/struktog/
+
+// Repo: https://github.com/archive-Basic-P-25-09/02a_Bestellung
+
+
+
+
